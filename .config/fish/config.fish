07b23e6828182258d4b1fdad68f61dfcdead8816
Timothée Desveaux <timothee.desveaux@epita.fr>
2021-07-28 20:35:45 +0200

    change some alias to abbreviation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,20 +8,18 @@ set fish_greeting
 # Aliases #
 ###########
 alias ls='ls --color=auto'
-alias la='ls -a'
-alias ll='ls -la'
+abbr la 'ls -a'
+abbr ll 'ls -la'
 
 abbr vim 'nvim'
 
-alias ccf='gcc -Wextra -Wall -Werror -std=c99 -pedantic -fsanitize=address -o main'
-alias cppf='g++ -Wextra -Wall -Werror -std=c++17 -pedantic -o main'
+abbr ccf 'gcc -Wextra -Wall -Werror -std=c99 -pedantic -fsanitize=address'
+abbr cppf 'g++ -Wextra -Wall -Werror -std=c++17 -pedantic'
 alias valgrind-full='valgrind --leak-check=full --show-leak-kinds=all --leak-resolution=high --track-origins=yes --vgdb=yes'
 
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
 
 alias pacman='pacman --color auto'
 
